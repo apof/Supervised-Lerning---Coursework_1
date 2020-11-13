101edd03b799fe65b89b59986a1ca4b09eaf0ace
apof <you@example.com>
2020-11-13 21:43:43 +0000

    delete unuseful comments in BostonRegression

--- BostonHousing.py
+++ BostonHousing.py
@@ -36,7 +36,6 @@ def linear_regression_fit(X,Y):
     return inv(X.T@X)@X.T@Y
 
 # (a) Predicting with the mean y-value on the training set
-# question 1.2.a
 def fit_with_constant_function(features_number,data_array,test_split,N):
 
 	train_std = 0
@@ -46,7 +45,7 @@ def fit_with_constant_function(features_number,data_array,test_split,N):
 
 	for i in range(0,N):
     
-		# perform a new data split on train and test set on every rou
+		# perform a new data split on train and test set on every round
 		train_data, test_data = data_split(data_array,test_split)
 
 		## initialize x vectors with ones - fit the data with constant function
@@ -56,35 +55,14 @@ def fit_with_constant_function(features_number,data_array,test_split,N):
 		y_test = np.asmatrix(test_data[:,(features_number-1)]).T
 			
 			
-		#print("Training set shape: " + str(x_train.shape))
-		#print(x_test.shape)
-		#print("Training labels shape: " + str(y_train.shape))
-		#print(y_test.shape)
-		#print("----")
-    
+    	## fit using simple linear regression formula
 		weights_train = linear_regression_fit(x_train,y_train)
-		#weights_test = linear_regression_fit(x_test,y_test)
 
-		#print(weights_train.shape)
-
-		#print("####")
-
-		#print("Weights shape: " + str(weights_train.shape))
-
+		#perform inference for the training and the test set
 		training_preds = x_train@weights_train
 		test_preds = x_test@weights_train
-
-		#print("Training predictions shape:  " + str(training_preds.shape))
-
-		#print(training_preds)
-		#print("-------")
-		#print(test_preds)
-		#print("_________")
-		#print(np.mean(y_train))
-		#print(np.mean(y_test))
-
-		#return
     
+		## compute the mean squaared error for the trining and the test set
 		mse_train[i]= mean_squared_error(training_preds,y_train)
 		mse_test[i]= mean_squared_error(test_preds,y_test)
 
@@ -96,8 +74,7 @@ def fit_with_constant_function(features_number,data_array,test_split,N):
 	print("Average MSE - std for train and test set are: " + str(np.mean(mse_train)) + " - " + str(train_std) +  " and " + str(np.mean(mse_test)) + " - " + str(test_std) + " respectively")
 
 
-    ## (b) Predicting with a single attribute and a bias term.
-
+## (b) Predicting with a single attribute and a bias term.
 def fit_with_one_feature(features_number,data_array,test_split,columns,N):
 
 	mse_train = np.zeros((N,(features_number-1)))
@@ -111,8 +88,6 @@ def fit_with_one_feature(features_number,data_array,test_split,columns,N):
         
 		for j in range(0,(features_number-1)):
 
-			#print("Fiting for feature: " + str(columns[j]))
-        
 			y_train = train_data[:,(features_number-1)].T
 			y_test = test_data[:,(features_number-1)].T
         
@@ -121,23 +96,12 @@ def fit_with_one_feature(features_number,data_array,test_split,columns,N):
 			x_train = np.asmatrix(np.c_[train_data [:,j],np.ones((train_data.shape[0],1))])
 			x_test = np.asmatrix(np.c_[test_data [:,j],np.ones((test_data.shape[0],1))])
 
-			#print("Training set shape: " + str(x_train.shape))
-			#print(x_test.shape)
-			#print("Training labels shape: " + str(y_train.shape))
-			#print(y_test.shape)
-			#print("----")
-        
+			
+			## linear regression fit
 			weights_train = linear_regression_fit(x_train,y_train)
 
-			#print("Weights shape: " + str(weights_train.shape))
-			
 			training_preds = x_train@(weights_train.T)
 			test_preds = x_test@(weights_train.T)
-
-			#print(np.mean(y_train))
-			#print(np.mena(training_preds))
-
-			#print("Training predictions shape:  " + str(training_preds.shape))
       
 			mse_train[i,j]= mean_squared_error(training_preds, y_train)
 			mse_test[i,j]= mean_squared_error(test_preds , y_test)
@@ -155,7 +119,7 @@ def fit_with_one_feature(features_number,data_array,test_split,columns,N):
 		print("Linear regression using attribute: " + str(columns[i]) + " MSE train - std: " + str(np.mean((np.sum(mse_train,axis = 0)/N).T[i])) + " - " + str(std_train[i]) + " MSE test - std:" + str(np.mean((np.sum(mse_test,axis = 0 )/N).T[i])) + " - " + str(std_test[i]))
 
 
- ## Predicting with all the features
+ ## (d) Predicting with all the features
 def fit_with_all_features(features_number,data_array,test_split,N):
 
 	mse_train = np.zeros(N)
@@ -175,22 +139,11 @@ def fit_with_all_features(features_number,data_array,test_split,N):
 		## and one extra column dimension with 1 values on the x
 		x_train = np.asmatrix(np.c_[train_data [:,range(features_number-1)],np.ones((train_data.shape[0],1))])
 		x_test = np.asmatrix(np.c_[test_data [:,range(features_number-1)],np.ones((test_data.shape[0],1))])
-
-
-		#print("Training set shape: " + str(x_train.shape))
-		#print(x_test.shape)
-		#print("Training labels shape: " + str(y_train.shape))
-		#print(y_test.shape)
-		#print("----")
         
 		weights_train = linear_regression_fit(x_train,y_train)
 
-		#print("Weights shape: " + str(weights_train.shape))
-			
 		training_preds = x_train@(weights_train.T)
 		test_preds = x_test@(weights_train.T)
-
-		#print("Training predictions shape:  " + str(training_preds.shape))
         
 		mse_train[i] = mean_squared_error(training_preds, y_train)
 		mse_test[i] = mean_squared_error(test_preds, y_test)
